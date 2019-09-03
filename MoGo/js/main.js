5e6d225fc56af5abb05aebf6f963a6c95c6d27ea
Romarios <romantgt@gmail.com>
2019-09-03 07:44:30 +0300

    commit

--- MoGo/js/main.js
+++ MoGo/js/main.js
@@ -9,18 +9,24 @@ $(document).ready(function() {
   $('#button-1').click(function() {
     $(this).toggleClass('active');
     $('#toggle-1').toggle('blind');
+    $('#button-2').removeClass('active');
+    $('#button-3').removeClass('active');
     $('#toggle-2').hide('blind');
     $('#toggle-3').hide('blind');
   });
   $('#button-2').click(function() {
     $(this).toggleClass('active');
     $('#toggle-2').toggle('blind');
+    $('#button-1').removeClass('active');
+    $('#button-3').removeClass('active');
     $('#toggle-1').hide('blind');
     $('#toggle-3').hide('blind');
   });
   $('#button-3').click(function() {
     $(this).toggleClass('active');
     $('#toggle-3').toggle('blind');
+    $('#button-2').removeClass('active');
+    $('#button-1').removeClass('active');
     $('#toggle-2').hide('blind');
     $('#toggle-1').hide('blind');
   });
