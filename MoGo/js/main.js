98e38cb723b63cb61e853ac553dd8af95386ae5d
RomariosGoIT <romantgt@gmail.com>
2019-09-04 16:28:10 +0300

    acc fixed

--- MoGo/js/main.js
+++ MoGo/js/main.js
@@ -2,7 +2,7 @@ const acc = document.querySelectorAll('.accordion');
 const panels = document.querySelectorAll('.panel');
 acc.forEach(item => item.addEventListener('click', toggleItem));
 function toggleItem() {
-  let panel = this.parentNode.nextElementSibling;
+  let panel = this.nextElementSibling;
   panels.forEach(item => item.classList.remove('open'));
   acc.forEach(item => item.classList.remove('active'));
   this.classList.add('active');
