f3a8f663374e49c404a4a01ed9c2ef860ae489c9
Romarios <romantgt@gmail.com>
2019-09-04 08:28:22 +0300

    commit

--- MoGo/js/main.js
+++ MoGo/js/main.js
@@ -1,59 +1,10 @@
-// $(document).ready(function() {
-//   $('#toggle-2').hide();
-//   $('#toggle-3').hide();
-
-//   if ($('#button-1').hasClass('active')) {
-//     $('#toggle-1').show('blind');
-//   }
-
-//   $('#button-1').click(function() {
-//     $(this).toggleClass('active');
-//     $('#toggle-1').toggle('blind');
-//     $('#button-2').removeClass('active');
-//     $('#button-3').removeClass('active');
-//     $('#toggle-2').hide('blind');
-//     $('#toggle-3').hide('blind');
-//   });
-//   $('#button-2').click(function() {
-//     $(this).toggleClass('active');
-//     $('#toggle-2').toggle('blind');
-//     $('#button-1').removeClass('active');
-//     $('#button-3').removeClass('active');
-//     $('#toggle-1').hide('blind');
-//     $('#toggle-3').hide('blind');
-//   });
-//   $('#button-3').click(function() {
-//     $(this).toggleClass('active');
-//     $('#toggle-3').toggle('blind');
-//     $('#button-2').removeClass('active');
-//     $('#button-1').removeClass('active');
-//     $('#toggle-2').hide('blind');
-//     $('#toggle-1').hide('blind');
-//   });
-// });
-
 const acc = document.querySelectorAll('.accordion');
 const panels = document.querySelectorAll('.panel');
-
 acc.forEach(item => item.addEventListener('click', toggleItem));
-
 function toggleItem() {
   let panel = this.parentNode.nextElementSibling;
-  panels.forEach(item => {
-    let button = item.previousElementSibling;
-
-    if (button.tagName == 'BUTTON') {
-      button.classList.remove('active');
-    }
-
-    item.classList.remove('open');
-
-    if (!item.classList.contains('open')) {
-      item.style.maxHeight = null;
-    }
-  });
-
+  panels.forEach(item => item.classList.remove('open'));
+  acc.forEach(item => item.classList.remove('active'));
   this.classList.add('active');
   panel.classList.add('open');
-  panel.style.maxHeight = panel.scrollHeight + 'px';
 }
